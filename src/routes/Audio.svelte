8726cb6c2f52e3cab3adf1cacc93f588ce7c5368
Naved Islam <navedcoded@gmail.com>
2022-11-11 20:10:33 +0800

    fix images

--- src/routes/Audio.svelte
+++ src/routes/Audio.svelte
@@ -197,7 +197,7 @@
 <!-- Checking of the author has no picture, then use default
 this is needed since vite doesn't play nice with onerror fallbacks -->
 {#if audios[currentTrack].author == 'الشيخ أبو مدين الغوث'}
-	<img src="/portraits/الله.jpg" alt="صورة الاسم" loading="lazy" />
+	<img id="author" src="/portraits/الله.jpg" alt="صورة الاسم" loading="lazy" />
 {:else}
 	<img
 		id="author"
