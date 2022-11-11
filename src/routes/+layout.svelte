4e76930095dced782b37b2926c151039d841df3d
Naved Islam <navedcoded@gmail.com>
2022-11-11 11:02:39 +0800

    footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,7 @@
 	</main>
 
 	<footer>
-		<!-- <p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p> -->
+		<p>دواوين العارفين بأنغام المحبين</p>
 	</footer>
 </div>
 
@@ -39,6 +39,7 @@
 		justify-content: center;
 		align-items: center;
 		padding: 12px;
+		color: var(--color-dim);
 	}
 
 	footer a {
